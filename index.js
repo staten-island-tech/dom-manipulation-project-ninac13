fb3358b4b77f807b25741c46fdb6ac1c974fba03
ninac13 <144480538+ninac13@users.noreply.github.com>
2024-10-11 13:57:56 -0400

    worked 10/11

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const DOMSelectors = {
     item: document.querySelector("li"),
     cardHeader: document.querySelector(".card-header"),
     card: document.querySelector(".card"),
-    button: document.querySelector(".btn"),
+    cardPrice: document.querySelector(".card-price"),
+    removeButton: document.querySelector(".removebtn"),
     form: document.querySelector(".form"),
 };
 
@@ -13,6 +14,11 @@ DOMSelectors.form.addEventListener("submit", function (event){
 });
 //create another function that can connect the user's 3 inputs to the background image/header/price of the card
 //create a function in that this can remove the card's background image, header, and price
-function removeCard(background, text){
-    background.image.
+function removeCard(){
+    DOMSelectors.card.remove();
 }
+
+// Call removeCard function when the remove button is clicked
+DOMSelectors.removeButton.addEventListener("click", function() {
+    removeCard();
+});
